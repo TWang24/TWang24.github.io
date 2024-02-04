68fb7e2dff44815f60b9e8c8255dedb798c172cf
TWang24 <wanggaorong@gmail.com>
2024-02-04 01:04:55 -0800

    add letter opening home page

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-const wrapper = document.querySelector('.wrapper');
-const question = document.querySelector('.question');
-const yesBtn = document.querySelector('.yes-btn');
-const noBtn = document.querySelector('.no-btn');
-const wrapperRect = wrapper.getBoundingClientRect();
-const noBtnRect = noBtn.getBoundingClientRect();
-yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'I Love You Too :)';
-});
-noBtn.addEventListener('mouseover', () => {
-    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
-    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
-    noBtn.style.left = i + 'px';
-    noBtn.style.top = j + 'px';
+const letterBtn = document.querySelector('.letter-image');
+
+letterBtn.addEventListener('click', () => {
+    location.href = "letter.html";
 });
